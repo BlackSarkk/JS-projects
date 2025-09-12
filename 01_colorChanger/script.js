882843677a356232df16fad1078fcf03c04b8a61
BlackSarkk <droncer22446688@gmail.com>
2025-09-12 10:17:18 +0530

    Complete the  colorChanger js project

--- 01_colorChanger/script.js
+++ 01_colorChanger/script.js
@@ -1,18 +1,21 @@
-const colorArr = [blue, green, pink, magenta, yellow, orange, violet, salmon, olive, peach]
+const colorArr = ['blue', 'green', 'orange', 'magenta', 'yellow', 'orange', 'violet', 'salmon', 'olive', 'lightgreen', "skyblue", "brown", "#f4c2c2"]
 const body = document.querySelector('body')
 const li = document.querySelectorAll('li')      // All is needed here without All forEach will throw error coz forEach expects an iterable object
 const ul = document.querySelector('ul')
 
 
 
-li.forEach((btn)=>{
-    btn.addEventListener("click", (e)=>{
+ul.addEventListener("click", (e)=>{
+    if(e.target.tagName==="LI"){
         body.style.backgroundColor = e.target.id
-    })
+    }
+}, false)
+
+
+
+colorArr.forEach((i)=>{
+    const newLi = document.createElement('li')
+    newLi.id = i
+    newLi.style.backgroundColor = i
+    ul.appendChild(newLi)
 })
-
-
-
-
-
-// now need to render buttons from that array 
